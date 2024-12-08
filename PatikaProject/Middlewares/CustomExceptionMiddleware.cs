1d51ca0795ae120a916a4943c309abd272279609
beyzzakurt <beyzzakurt34@gmail.com>
2024-12-08 23:48:02 +0300

    Validation added

---
+++ PatikaProject/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace PatikaProject.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+        }
+    }
+}
